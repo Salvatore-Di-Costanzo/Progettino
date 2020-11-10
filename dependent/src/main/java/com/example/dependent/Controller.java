67fd369ce18f564caaf3fda0624f07caa89db498
EmanueleIandolo123 <iandoloemanuele7@gmail.com>
2020-11-10 16:20:01 +0100

    Speriamo che funziona

--- dependent/src/main/java/com/example/dependent/Controller.java
+++ dependent/src/main/java/com/example/dependent/Controller.java
@@ -4,6 +4,7 @@ package com.example.dependent;
 import com.example.dependent.Pojo.Dependent;
 import com.example.dependent.Repository.DependentRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +16,7 @@ public class Controller {
     @Autowired
     DependentRepo dependentRepo;
 
-    @GetMapping("/getDependent")
+    //@GetMapping("/getDependent")
 
     @GetMapping("/getDependents")
     public List<Dependent> getDependents (){
--- dependent/src/main/resources/data.sql
+++ dependent/src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO DEPENDENT VALUES (1,"Salvatore","Di Costanzo","12000");
-INSERT INTO DEPENDENT VALUES (2,"Emanuele","Iandolo","12000");
+INSERT INTO DEPENDENT VALUES (1,'Salvatore','Di Costanzo',12000);
+INSERT INTO DEPENDENT VALUES (2,'Emanuele','Iandolo',12000);
