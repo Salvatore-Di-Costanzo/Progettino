5f51584ce48f76d5dae76fa952b4c24bbf793c28
Salvatore Di Costanzo <dicostanzosalvatore@yahoo.it>
2020-11-10 15:20:28 +0100

    Initial commit

---
+++ turni/src/main/java/com/example/turni/TurniApplication.java
@@ -0,0 +1,13 @@
+package com.example.turni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurniApplication.class, args);
+	}
+
+}
