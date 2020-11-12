c5721837985a833411c07f33f0ddaa5f75dea5aa
Salvatore Di Costanzo <dicostanzosalvatore@yahoo.it>
2020-11-12 12:21:50 +0100

    Aggiunto algoritmo turni e retrive turni da data con Mysql

--- turni/src/main/java/com/example/turni/controller/RealizzaTurni.java
+++ turni/src/main/java/com/example/turni/controller/RealizzaTurni.java
@@ -3,6 +3,7 @@ package com.example.turni.controller;
 import com.example.turni.pojo.Turno;
 import lombok.extern.slf4j.Slf4j;
 import org.hibernate.Session;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
@@ -30,43 +31,57 @@ public class RealizzaTurni {
         this.entityManager = entityManager;
     }
 
-    public String calcolaTurni(int numGiorni){
+    public String calcolaTurni(int numGiorni) {
+
+        countDays = 0;
+
+
+        LocalDate baseDate = LocalDate.parse(getMaxDate());
+        LocalDate setDate ;
+
+        log.info("\n\nData del db: " + baseDate.toString() + "\n\n");
 
         /// Importo gli Id dei dipendenti
         List<Integer> idDependent = feignDependent.getIds();
 
         List<Integer> idGiornata = new ArrayList<>();
-        idGiornata.clear();
 
         StringBuilder dataFine = new StringBuilder();
 
         /// Algoritmo generazione turni
-        for(int i = 1; i <= numGiorni * 4; i++) {
+        for (int i = 1; i <= numGiorni * 4; i++) {
 
             // Calcolo la data
+            if (i == 1 && baseDate.isBefore(LocalDate.now())) baseDate = LocalDate.now();
+
 
             // Se il giorno è Sabato o Domenica saltalo
-            while ((LocalDate.now().plusDays(countDays).getDayOfWeek() == DayOfWeek.SATURDAY ||
-                    LocalDate.now().plusDays(countDays).getDayOfWeek() == DayOfWeek.SUNDAY))
-                countDays++;
-            LocalDate setDate = LocalDate.now().plusDays(countDays);
-
+            if (baseDate.plusDays(countDays).getDayOfWeek() == DayOfWeek.SATURDAY) countDays += 2;
+            if (i==1 && LocalDate.now().isBefore(baseDate)) countDays++;
+            setDate = baseDate.plusDays(countDays);
 
             // Verifico se devo avanzare con la data, poichè il numero max di dipendenti è 4 per ogni giorno
             // ogni qualvolta elaboro 4 dipendenti passo al giorno dopo.
-            if (i%4 == 0 ) {
+            if (i % 4 == 0) {
                 countDays++;
                 idGiornata.clear();
             }
 
+
             // Mappo il turno da inserire
             Turno turno = new Turno();
             StringBuilder data = new StringBuilder();
-            data.append(setDate.getDayOfMonth());
-            data.append("-");
-            data.append(setDate.getMonthValue());
-            data.append("-");
             data.append(setDate.getYear());
+            data.append("-");
+            if (setDate.getMonthValue() >= 1 && setDate.getMonthValue() <= 9)
+                data.append("0"+setDate.getMonthValue());
+            else
+                data.append(setDate.getMonthValue());
+            data.append("-");
+            if (setDate.getDayOfMonth() >= 1 && setDate.getDayOfMonth() <= 9)
+                data.append("0" + setDate.getDayOfMonth());
+            else
+                data.append(+setDate.getDayOfMonth());
             log.info("Data caricata: " + data.toString());
             turno.setDate(data.toString());
             int randomIdex = ThreadLocalRandom.current().nextInt(0, idDependent.size());
@@ -82,9 +97,8 @@ public class RealizzaTurni {
             idDependent.remove(randomIdex);
 
 
-
             /// Verifico che la lista non debba essere ricaricata
-            if (idDependent.isEmpty()){
+            if (idDependent.isEmpty()) {
                 idDependent.addAll(feignDependent.getIds());
                 idDependent.removeAll(idGiornata);
             }
@@ -97,30 +111,49 @@ public class RealizzaTurni {
             currentSession.save(turno);
             currentSession.getTransaction().commit();
 
-            if (i == numGiorni * 4){
-                dataFine.append(setDate.getDayOfMonth());
+            if (i == numGiorni * 4) {
+                dataFine.append(setDate.getYear());
                 dataFine.append("-");
                 dataFine.append(setDate.getMonthValue());
                 dataFine.append("-");
-                dataFine.append(setDate.getYear());
+                dataFine.append(setDate.getDayOfMonth());
             }
 
         }
 
-        LocalDate setDate = LocalDate.now();
+        //LocalDate setDate = LocalDate.now();
 
         StringBuilder dataInizio = new StringBuilder();
-        dataInizio.append(setDate.getDayOfMonth());
+
+        dataInizio.append(baseDate.getYear());
         dataInizio.append("-");
-        dataInizio.append(setDate.getMonthValue());
+        dataInizio.append(baseDate.getMonthValue());
         dataInizio.append("-");
-        dataInizio.append(setDate.getYear());
-
-
-
+        dataInizio.append(baseDate.getDayOfMonth());
 
 
         return "Calcolo turni effettuato dal: " + dataInizio.toString() + " al: " + dataFine.toString();
+
+    }
+
+    public String getMaxDate() {
+
+        Session currentSession = entityManager.unwrap(Session.class);
+
+        Query idMax =
+                currentSession.createQuery("select max(id) from Turno", Integer.class);
+
+        if (idMax.getSingleResult() == null)
+            return "1900-01-01";
+
+        Query theQuery =
+                currentSession.createQuery("from Turno where id = (select max(id) from Turno)", Turno.class);
+
+
+        List<Turno> turno = theQuery.getResultList();
+
+        return turno.get(0).getDate();
+
 
     }
 
